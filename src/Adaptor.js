92ab8318708d77be061dec5bdd465ee9a3f41e8c
Stuart Corbishley <corbish@gmail.com>
2021-05-18 15:35:57 +0200

    Doc change to test git log

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -36,7 +36,7 @@ export function execute(...operations) {
 }
 
 /**
- * Creates a resource in a destination system with a POST request
+ * Creates a resource in a destination system using a POST request
  * @public
  * @example
  * create("/endpoint", {"foo": "bar"})
